405b87c1863dcf40d7114eed90e19bc588af7255
Doris Chen <43398278+dsmchen@users.noreply.github.com>
2026-02-25 13:24:57 +0800

    fix: rollback to start of lesson 6

--- src/pages/careers/Careers.js
+++ src/pages/careers/Careers.js
@@ -1,23 +1,3 @@
-import { Link, useLoaderData } from "react-router-dom"
-
 export default function Careers() {
-  const careers = useLoaderData()
-
-  return (
-    <div className="careers">
-      {careers.map(career => (
-        <Link to='/' key={career.id}>
-          <p>{career.title}</p>
-          <p>Based in {career.location}</p>
-        </Link>
-      ))}
-    </div>
-  )
-}
-
-// data loader
-export const careersLoader = async () => {
-  const res = await fetch('http://localhost:4000/careers')
-
-  return res.json()
+  return <div className="careers"></div>;
 }
