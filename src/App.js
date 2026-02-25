30179039c0963246d7784e60e1142e59f0274e16
Doris Chen <43398278+dsmchen@users.noreply.github.com>
2026-02-25 14:55:48 +0800

    feat: add route params

--- src/App.js
+++ src/App.js
@@ -12,6 +12,7 @@ import Faq from './pages/help/Faq';
 import Contact from './pages/help/Contact';
 import NotFound from './pages/NotFound';
 import Careers, { careersLoader } from './pages/careers/Careers';
+import CareerDetails from './pages/careers/CareerDetails';
 
 // layouts
 import RootLayout from './layouts/RootLayout';
@@ -29,6 +30,7 @@ const router = createBrowserRouter(
       </Route>
       <Route path="careers" element={<CareersLayout />}>
         <Route index element={<Careers />} loader={careersLoader} />
+        <Route path=":id" element={<CareerDetails />} />
       </Route>
 
       <Route path="*" element={<NotFound />} />
